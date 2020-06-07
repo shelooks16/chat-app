e84f6f16e12e85ecedd372e1a0a57cfc80876a3f
shelooks16 <blauyedz@zoho.eu>
2020-06-07 16:44:34 +0300

    manage user with profile context; put real time subs

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import SignIn from './pages/SignIn';
 import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
 import PublicRoute from './components/PublicRoute';
+import { ProfileProvider } from './context/profile.context';
 
 function App() {
   return (
-    <Switch>
-      <PublicRoute path="/signin">
-        <SignIn />
-      </PublicRoute>
-      <PrivateRoute path="/">
-        <Home />
-      </PrivateRoute>
-    </Switch>
+    <ProfileProvider>
+      <Switch>
+        <PublicRoute path="/signin">
+          <SignIn />
+        </PublicRoute>
+        <PrivateRoute path="/">
+          <Home />
+        </PrivateRoute>
+      </Switch>
+    </ProfileProvider>
   );
 }
 
--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router';
+import { Container, Loader } from 'rsuite';
+import { useProfile } from '../context/profile.context';
 
 const PublicRoute = ({ children, ...routeProps }) => {
-  const profile = false;
+  const { profile, isLoading } = useProfile();
 
-  if (profile) {
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" content="Loading" speed="slow" />
+      </Container>
+    );
+  }
+
+  if (profile && !isLoading) {
     return <Redirect to="/" />;
   }
 
