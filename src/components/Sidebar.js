5817c824b807f692b572deb181f6e834a843dff0
shelooks16 <blauyedz@zoho.eu>
2020-06-09 17:16:12 +0300

    add button to create new chat room

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import DashboardToggle from './dashboard/DashboardToggle';
+import CreateRoomBtnModal from './CreateRoomBtnModal';
 
 const Sidebar = () => {
   return (
     <div className="h-100 pt-2">
       <div>
         <DashboardToggle />
+        <CreateRoomBtnModal />
       </div>
       bottom
     </div>
