3bf80fa110bc4d97983bd68045b22a263ad961f0
shelooks16 <blauyedz@zoho.eu>
2020-06-10 14:02:09 +0300

    started to work with chat room ;iost

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,8 @@ import Sidebar from '../components/Sidebar';
 const Home = () => {
   return (
     <Grid fluid className="h-100">
-      <Row>
-        <Col xs={24} md={8}>
+      <Row className="h-100">
+        <Col xs={24} md={8} className="h-100">
           <Sidebar />
         </Col>
       </Row>
