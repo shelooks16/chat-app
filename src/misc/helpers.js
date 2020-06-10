4988499cbeb90eec336ff0c9690d52af7b8eeabd
shelooks16 <blauyedz@zoho.eu>
2020-06-10 15:55:21 +0300

    created rooms context

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -7,3 +7,11 @@ export function getNameInitials(name) {
 
   return splitName[0][0];
 }
+
+export function transformToArrWithId(snapVal) {
+  return snapVal
+    ? Object.keys(snapVal).map(roomId => {
+        return { ...snapVal[roomId], id: roomId };
+      })
+    : [];
+}
