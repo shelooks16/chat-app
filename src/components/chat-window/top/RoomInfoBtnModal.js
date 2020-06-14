c47de16907aadf20242501e7e9d9467c6af5ba16
shelooks16 <blauyedz@zoho.eu>
2020-06-14 16:25:17 +0300

    created chat bottom; send messages to the chat

--- src/components/chat-window/top/RoomInfoBtnModal.js
+++ src/components/chat-window/top/RoomInfoBtnModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Button, Modal } from 'rsuite';
 import { useCurrentRoom } from '../../../context/current-room.context';
 import { useModalState } from '../../../misc/custom-hooks';
@@ -31,4 +31,4 @@ const RoomInfoBtnModal = () => {
   );
 };
 
-export default RoomInfoBtnModal;
+export default memo(RoomInfoBtnModal);
--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -12,7 +12,7 @@ const Top = () => {
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
-        <h4>
+        <h4 className="text-disappear d-flex align-items-center">
           <Icon
             componentClass={Link}
             to="/"
