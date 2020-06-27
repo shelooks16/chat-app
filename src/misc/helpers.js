3eb9ce5104e52ba0177be9b31f0b8c116cad0459
shelooks16 <blauyedz@zoho.eu>
2020-06-27 16:49:56 +0300

    added service worker

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -63,3 +63,13 @@ export function groupBy(array, groupingKeyFn) {
     return result;
   }, {});
 }
+
+export const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.0/8 are considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
